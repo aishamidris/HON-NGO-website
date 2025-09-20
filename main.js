8e5f9583293101f832712e2e8fe8f7a9689fabcf
Aisha Muhammad <aishamuhammadidris92@gmail.com>
2025-09-20 14:10:03 +0100

    index.html mobile responsiveness fixed

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar =document.querySelector(".navbar");
+
+menuToggle.addEventListener("click",function() {
+    navbar.classList.toggle("active");
+} );
